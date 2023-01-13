88969a9c2191b53caef68e11cbda4e39ea331f8d
Darwin Perdomo <ZeroBl@hotmail.com>
2023-01-12 23:24:28 -0400

    feat(controllers/post): Add update and show single endpoint

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -32,6 +32,20 @@ export const getPosts = async (req, res, next) => {
   }
 }
 
+export const getPost = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const post = await Post.findById(id).populate('author', ['username'])
+    if (!post) {
+      throw handleError(404, 'Could not find post.')
+    }
+
+    res.status(200).json({ message: 'Post Fetched', post })
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const createPost = async (req, res, next) => {
   if (!req.file) {
     throw handleError(422, 'No image provided.')
@@ -51,6 +65,38 @@ export const createPost = async (req, res, next) => {
     })
 
     res.status(201).json({ message: 'Post has been created', post })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updatePost = async (req, res, next) => {
+  const newPath = req?.file?.path
+
+  const { id, title, summary, content } = req.body
+  const { token } = req.cookies
+
+  try {
+    const user = await jwt.verify(token, process.env.SECRET_CODE)
+
+    const post = await Post.findById(id)
+    if (!post) {
+      throw handleError(404, 'Could not find post.')
+    }
+
+    const isAuthor = post.author.toString() === user.userId
+    if (!isAuthor) {
+      throw handleError(401, 'you are not the author')
+    }
+
+    await post.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath || post.cover
+    })
+
+    res.json({ message: 'Post has been updated', post })
   } catch (error) {
     next(error)
   }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 
-import { createPost, getPosts } from '../controllers/post.js'
+import {
+  createPost,
+  getPost,
+  getPosts,
+  updatePost
+} from '../controllers/post.js'
 
 const router = Router()
 
 router.get('/post', getPosts)
+router.get('/post/:id', getPost)
+router.put('/post/:id', updatePost)
 router.post('/post', createPost)
 
 export default router
